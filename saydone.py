62498a8684373e72c3640aaf9a45913586cf0c08
yifengyou <842056007@qq.com>
2023-07-03 22:27:25 +0800

    添加选项参数和配置文件

--- saydone.py
+++ saydone.py
@@ -5,16 +5,25 @@
  Authors:
    yifengyou <842056007@qq.com>
 """
-
+import argparse
 import os
 import datetime
+import sys
 
-# 有名管道的路径
+CURRENT_VERSION = "0.1.0"
 PIPE_PATH = "/var/log/saydone"
 LOG_PATH = "/var/log/saydone.txt"
+DEBUG = False
 
 
 # PROMPT_COMMAND='if [ -e /var/log/saydone ]; then echo "$? $USER `fc -ln -0`" > /var/log/saydone ; fi'
+def check_python_version():
+    current_python = sys.version_info[0]
+    if current_python == 3:
+        return
+    else:
+        raise Exception('Invalid python version requested: %d' % current_python)
+
 
 def timestamp():
     utc_time = datetime.datetime.utcnow()
@@ -23,7 +32,7 @@ def timestamp():
     return beijing_time.strftime("%Y/%m/%d %H:%M:%S")
 
 
-if __name__ == "__main__":
+def handle_daemon(args):
     if not os.path.exists(PIPE_PATH):
         os.mkfifo(PIPE_PATH)
         os.chmod(PIPE_PATH, 0o666)
@@ -45,3 +54,51 @@ if __name__ == "__main__":
             log_file.flush()
     # else:
     #     exit(0)
+    print(" handle daemon done!")
+
+
+def handle_stop(args):
+    print(" handle stop done!")
+
+
+def handle_start(args):
+    print(" handle start done!")
+
+
+def main():
+    global DEBUG, CURRENT_VERSION
+    check_python_version()
+
+    # 顶层解析
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("-v", "--version", action="store_true",
+                        help="show program's version number and exit")
+    parser.add_argument("-h", "--help", action="store_true",
+                        help="show this help message and exit")
+    subparsers = parser.add_subparsers()
+
+    # 添加子命令 start
+    parser_start = subparsers.add_parser('start')
+    parser_start.set_defaults(func=handle_start)
+    # 添加子命令 stop
+    parser_stop = subparsers.add_parser('stop')
+    parser_stop.set_defaults(func=handle_stop)
+    # 添加子命令 daemon
+    parser_daemon = subparsers.add_parser('daemon')
+    parser_daemon.set_defaults(func=handle_daemon)
+
+    # 开始解析命令
+    args = parser.parse_args()
+
+    if args.version:
+        print("saydone %s" % CURRENT_VERSION)
+        sys.exit(0)
+    elif args.help or len(sys.argv) < 2:
+        parser.print_help()
+        sys.exit(0)
+    else:
+        args.func(args)
+
+
+if __name__ == "__main__":
+    main()
